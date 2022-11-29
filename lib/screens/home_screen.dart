64db29957c2237d1bd1e9aeebcf9438b10cbf1d0
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-11-29 11:12:54 +0100

    Cleaning the app : remove the app bar and the debug mode

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,34 +9,40 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: const Color(0xFFeeedf2),
       body: ListView(
         children: [
-          Column(
-            children: [
-              Row(
-                children: [
-                  Column(
-                    children: const [
-                      Text(
-                        "Good morning",
-                      ),
-                      Text(
-                        "Book Tickets",
-                      ),
-                    ],
-                  ),
-                  Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: const DecorationImage(
-                        fit: BoxFit.fitHeight,
-                        image: AssetImage("assets/images/img_1.png"),
+          Container(
+            padding: const EdgeInsets.symmetric(
+                horizontal: 20), // Padding left & right
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Good morning",
+                        ),
+                        Text(
+                          "Book Tickets",
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          fit: BoxFit.fitHeight,
+                          image: AssetImage("assets/images/img_1.png"),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           )
         ],
       ),
