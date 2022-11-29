030304c3f57f6c6f301ed0132c67d7e68df90437
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-11-29 11:43:19 +0100

    Extracting style and Apply it to background color and text in home_screen.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:booktickets/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:booktickets/screens/bottom_bar.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: primary,
       ),
       home: const BottomBar(),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:booktickets/utils/app_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFeeedf2),
+      backgroundColor: Styles.bgColor,
       body: ListView(
         children: [
           Container(
@@ -19,12 +21,15 @@ class HomeScreen extends StatelessWidget {
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
+                      children: [
                         Text(
                           "Good morning",
+                          style: Styles.headLineStyle3,
                         ),
+                        const Gap(5), // its like SizedBox(height: 5)
                         Text(
                           "Book Tickets",
+                          style: Styles.headLineStyle1,
                         ),
                       ],
                     ),
