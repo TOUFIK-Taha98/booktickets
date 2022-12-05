35903c46459c35bd66dc494ff4e3ede0d67c1456
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-12-05 16:24:05 +0100

    Cleaning code

--- lib/screens/hotel_screen.dart
+++ lib/screens/hotel_screen.dart
@@ -13,12 +13,12 @@ class HotelScreen extends StatelessWidget {
     final size = AppLayout.getSize(context);
     return Container(
       width: size.width * 0.6,
-      height: 350,
+      height: AppLayout.getHeight(350),
       padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 17),
       margin: const EdgeInsets.only(right: 17, top: 5),
       decoration: BoxDecoration(
         color: Styles.primaryColor,
-        borderRadius: BorderRadius.circular(24),
+        borderRadius: BorderRadius.circular(AppLayout.getHeight(24)),
         boxShadow: [
           BoxShadow(
               color: Colors.grey.shade200, blurRadius: 20, spreadRadius: 5),
@@ -28,9 +28,9 @@ class HotelScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            height: 180,
+            height: AppLayout.getHeight(180),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
+              borderRadius: BorderRadius.circular(AppLayout.getHeight(12)),
               color: Styles.primaryColor,
               image: DecorationImage(
                 fit: BoxFit.cover,
--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -14,20 +14,20 @@ class TicketView extends StatelessWidget {
     final size = AppLayout.getSize(context);
     return SizedBox(
       width: size.width * 0.85,
-      height: 200,
+      height: AppLayout.getHeight(200),
       child: Container(
-        margin: const EdgeInsets.only(right: 16),
+        margin: EdgeInsets.only(right: AppLayout.getHeight(16)),
         child: Column(
           children: [
             // Showing the blue part of the card/ticket
             Container(
-              decoration: const BoxDecoration(
+              decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(21),
-                    topRight: Radius.circular(21)),
-                color: Color(0xFF526799),
+                    topLeft: Radius.circular(AppLayout.getHeight(21)),
+                    topRight: Radius.circular(AppLayout.getHeight(21))),
+                color: const Color(0xFF526799),
               ),
-              padding: const EdgeInsets.all(16),
+              padding: EdgeInsets.all(AppLayout.getHeight(16)),
               child: Column(
                 children: [
                   Row(
@@ -44,7 +44,7 @@ class TicketView extends StatelessWidget {
                       Expanded(
                         child: Stack(children: [
                           SizedBox(
-                            height: 24,
+                            height: AppLayout.getHeight(24),
                             child: LayoutBuilder(
                               builder: (BuildContext context,
                                   BoxConstraints constraints) {
@@ -57,10 +57,10 @@ class TicketView extends StatelessWidget {
                                   mainAxisSize: MainAxisSize.max,
                                   children: List.generate(
                                     (constraints.constrainWidth() / 6).floor(),
-                                    (index) => const SizedBox(
-                                      width: 3,
-                                      height: 1,
-                                      child: DecoratedBox(
+                                    (index) => SizedBox(
+                                      width: AppLayout.getWidth(3),
+                                      height: AppLayout.getHeight(1),
+                                      child: const DecoratedBox(
                                           decoration: BoxDecoration(
                                               color: Colors.white)),
                                     ),
@@ -93,7 +93,7 @@ class TicketView extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       SizedBox(
-                        width: 100,
+                        width: AppLayout.getWidth(100),
                         child: Text(
                           ticket['from']['name'],
                           style: Styles.headLineStyle4
@@ -106,7 +106,7 @@ class TicketView extends StatelessWidget {
                             Styles.headLineStyle4.copyWith(color: Colors.white),
                       ),
                       SizedBox(
-                        width: 100,
+                        width: AppLayout.getWidth(100),
                         child: Text(
                           ticket['to']['name'],
                           textAlign: TextAlign.end,
@@ -123,15 +123,15 @@ class TicketView extends StatelessWidget {
             Container(
               color: Styles.orangeColor,
               child: Row(children: [
-                const SizedBox(
-                  height: 20,
-                  width: 10,
+                SizedBox(
+                  height: AppLayout.getHeight(20),
+                  width: AppLayout.getWidth(10),
                   child: DecoratedBox(
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(10),
-                        bottomRight: Radius.circular(10),
+                        topRight: Radius.circular(AppLayout.getHeight(10)),
+                        bottomRight: Radius.circular(AppLayout.getHeight(10)),
                       ),
                     ),
                   ),
@@ -148,10 +148,10 @@ class TicketView extends StatelessWidget {
                           mainAxisSize: MainAxisSize.max,
                           children: List.generate(
                               (constraints.constrainWidth() / 15).floor(),
-                              (index) => const SizedBox(
-                                    width: 5,
-                                    height: 1,
-                                    child: DecoratedBox(
+                              (index) => SizedBox(
+                                    width: AppLayout.getWidth(5),
+                                    height: AppLayout.getHeight(1),
+                                    child: const DecoratedBox(
                                       decoration:
                                           BoxDecoration(color: Colors.white),
                                     ),
@@ -161,15 +161,15 @@ class TicketView extends StatelessWidget {
                     ),
                   ),
                 ),
-                const SizedBox(
-                  height: 20,
-                  width: 10,
+                SizedBox(
+                  height: AppLayout.getHeight(20),
+                  width: AppLayout.getHeight(10),
                   child: DecoratedBox(
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        bottomLeft: Radius.circular(10),
+                        topLeft: Radius.circular(AppLayout.getHeight(10)),
+                        bottomLeft: Radius.circular(AppLayout.getHeight(10)),
                       ),
                     ),
                   ),
@@ -179,9 +179,9 @@ class TicketView extends StatelessWidget {
             // The bottom part of the card/ticket
             Container(
               decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(21),
-                    bottomRight: Radius.circular(21)),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(AppLayout.getHeight(21)),
+                    bottomRight: Radius.circular(AppLayout.getHeight(21))),
                 color: Styles.orangeColor,
               ),
               padding: const EdgeInsets.only(
