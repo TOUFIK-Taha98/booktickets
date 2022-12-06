1501923eed281cfe4543a2769ef43c8430f5f648
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-12-06 10:51:47 +0100

    wrap container widget with stack widget

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:booktickets/utils/app_layout.dart';
 import 'package:booktickets/utils/app_styles.dart';
+import 'package:booktickets/widgets/double_text_widget.dart';
 import 'package:booktickets/widgets/icon_text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -91,6 +92,150 @@ class SearchScreen extends StatelessWidget {
               ),
             ),
           ),
+          Gap(AppLayout.getHeight(15)),
+          const AppDoubleTextWidget(
+            bigText: "Upcoming Flight",
+            smallText: "View all",
+          ),
+          Gap(AppLayout.getHeight(40)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: AppLayout.getHeight(425),
+                width: size.width * 0.42,
+                padding: EdgeInsets.symmetric(
+                    horizontal: AppLayout.getHeight(15),
+                    vertical: AppLayout.getHeight(15)),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(
+                    AppLayout.getHeight(20),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.shade200,
+                        blurRadius: 1,
+                        spreadRadius: 1),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: AppLayout.getHeight(190),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                          AppLayout.getHeight(12),
+                        ),
+                        image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/images/sit.jpg"),
+                        ),
+                      ),
+                    ),
+                    Gap(AppLayout.getHeight(12)),
+                    Text(
+                      "20% discout on the early booking of the flight. Don't miss this opportunity",
+                      style: Styles.headLineStyle2,
+                    )
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: size.width * 0.44,
+                        height: AppLayout.getHeight(200),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF3ABBBB),
+                          borderRadius: BorderRadius.circular(
+                            AppLayout.getHeight(18),
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          vertical: AppLayout.getHeight(15),
+                          horizontal: AppLayout.getHeight(15),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Discount\nfor survery",
+                              style: Styles.headLineStyle2
+                                  .copyWith(color: Colors.white),
+                            ),
+                            Gap(AppLayout.getHeight(10)),
+                            Text(
+                              "Take the survery about our services and get discount",
+                              style: Styles.headLineStyle2.copyWith(
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        right: -45,
+                        top: -45,
+                        child: Container(
+                          padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 18,
+                              color: const Color(0xFF189999),
+                            ),
+                            color: Colors.transparent,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Gap(AppLayout.getHeight(15)),
+                  Container(
+                    width: size.width * 0.44,
+                    height: AppLayout.getHeight(210),
+                    padding: EdgeInsets.symmetric(
+                      vertical: AppLayout.getHeight(15),
+                      horizontal: AppLayout.getHeight(15),
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.circular(AppLayout.getHeight(18)),
+                      color: const Color(0xFFEC6545),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Take love",
+                          style: Styles.headLineStyle2.copyWith(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        Gap(AppLayout.getHeight(5)),
+                        RichText(
+                          text: const TextSpan(
+                            children: [
+                              TextSpan(
+                                  text: '😃', style: TextStyle(fontSize: 38)),
+                              TextSpan(
+                                  text: "😅", style: TextStyle(fontSize: 50)),
+                              TextSpan(
+                                  text: "😊", style: TextStyle(fontSize: 38)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ],
+          )
         ],
       ),
     );
