a9724a9f1f72f445cefee1bd2afe2281d236d0f8
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-12-07 09:57:09 +0100

    header done + background and the circular on the section 1

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,7 +1,8 @@
+import 'package:booktickets/utils/app_layout.dart';
 import 'package:booktickets/utils/app_styles.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:gap/gap.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -11,7 +12,135 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Styles.bgColor,
       body: ListView(
-        children: [],
+        padding: EdgeInsets.symmetric(
+          horizontal: AppLayout.getHeight(20),
+          vertical: AppLayout.getHeight(20),
+        ),
+        children: [
+          Gap(AppLayout.getHeight(40)),
+          // header section
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //logo
+              Container(
+                height: AppLayout.getHeight(86),
+                width: AppLayout.getHeight(86),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      AppLayout.getHeight(10),
+                    ),
+                    image: const DecorationImage(
+                      image: AssetImage("assets/images/img_1.png"),
+                    )),
+              ),
+              Gap(AppLayout.getHeight(10)),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Book Tickets",
+                    style: Styles.headLineStyle1,
+                  ),
+                  Gap(AppLayout.getHeight(2)),
+                  Text(
+                    "New-York",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                  Gap(AppLayout.getHeight(8)),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        AppLayout.getHeight(100),
+                      ),
+                      color: const Color(0xFFFEF4F3),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: AppLayout.getHeight(3),
+                      vertical: AppLayout.getHeight(3),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(AppLayout.getHeight(3)),
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFF526799),
+                          ),
+                          child: const Icon(
+                            FluentSystemIcons.ic_fluent_shield_filled,
+                            color: Colors.white,
+                            size: 15,
+                          ),
+                        ),
+                        Gap(AppLayout.getHeight(5)),
+                        const Text(
+                          "Premium status",
+                          style: TextStyle(
+                              color: Color(0xFF526799),
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Gap(AppLayout.getHeight(5)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Column(
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      "Edit",
+                      style: Styles.textStyle.copyWith(
+                          color: Styles.primaryColor,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+          Gap(AppLayout.getHeight(8)),
+          Divider(color: Colors.grey.shade300),
+          // second section
+          Stack(
+            children: [
+              //background
+              Container(
+                height: AppLayout.getHeight(90),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Styles.primaryColor,
+                  borderRadius: BorderRadius.circular(
+                    AppLayout.getHeight(18),
+                  ),
+                ),
+              ),
+              // cirle shape
+              Positioned(
+                right: -45,
+                top: -40,
+                child: Container(
+                  padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      width: 18,
+                      color: const Color(0xFF264CD2),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
